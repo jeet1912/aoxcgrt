4ddef98405fc1ea773c8c68cabab9f00cbdb0b71
Jeet1912 <sahdevjeet@gmail.com>
2024-06-07 11:56:49 -0400

    updated requirements.txt

--- code/requirementsHelper.py
+++ code/requirementsHelper.py
@@ -1,8 +1,9 @@
-## Generated using Copilot
-## This script converts a Jupyter notebook to a requirements.txt file
+#Generated using GPT4o.
+
 import ast
 import nbformat
 from nbconvert import PythonExporter
+import os
 
 def get_imports(node):
     if isinstance(node, ast.Import):
@@ -12,25 +13,31 @@ def get_imports(node):
     else:
         return []
 
-def notebook_to_requirements(notebook_path, requirements_path):
-    # Convert the Jupyter notebook to a Python script
-    with open(notebook_path) as f:
-        nb = nbformat.read(f, as_version=4)
-    exporter = PythonExporter()
-    python_script, _ = exporter.from_notebook_node(nb)
+def get_top_level_package(module_name):
+    return module_name.split('.')[0]
 
-    # Parse the Python script
-    module = ast.parse(python_script)
+def notebook_to_requirements(directory_path, requirements_path):
+    libraries = set()
 
-    # Extract the libraries
-    libraries = []
-    for node in ast.walk(module):
-        libraries.extend(get_imports(node))
+    for root, _, files in os.walk(directory_path):
+        for file in files:
+            if file.endswith('.ipynb'):
+                notebook_path = os.path.join(root, file)
+                with open(notebook_path) as f:
+                    nb = nbformat.read(f, as_version=4)
+                exporter = PythonExporter()
+                python_script, _ = exporter.from_notebook_node(nb)
 
-    # Write the libraries to the requirements.txt file
+                module = ast.parse(python_script)
+
+                for node in ast.walk(module):
+                    imports = get_imports(node)
+                    top_level_imports = [get_top_level_package(lib) for lib in imports]
+                    libraries.update(top_level_imports)
+
     with open(requirements_path, 'w') as f:
-        for library in sorted(set(libraries)):
+        for library in sorted(libraries):
             f.write(f'{library}\n')
 
 # Usage
-notebook_to_requirements('your_notebook.ipynb', 'requirements.txt')
+notebook_to_requirements('code', 'requirements.txt')
